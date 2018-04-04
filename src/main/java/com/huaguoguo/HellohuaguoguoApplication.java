de1ea2c69f27f60ade507ea7273bc2bf0a0dcd48
huaguoguo555 <huoguoguo404@163.com>
2018-04-04 18:16:05 +0800

    哈哈哈

--- src/main/java/com/huaguoguo/HellohuaguoguoApplication.java
+++ src/main/java/com/huaguoguo/HellohuaguoguoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HellohuaguoguoApplication {
 
+	//哈哈哈
 	public static void main(String[] args) {
 		SpringApplication.run(HellohuaguoguoApplication.class, args);
 	}
