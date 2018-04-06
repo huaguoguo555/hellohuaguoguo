f06583c8071f85032a655ddea76b209873520367
huaguoguo <240857521@qq.com>
2018-04-06 21:43:52 +0800

    加入了日志

--- src/main/java/com/huaguoguo/HellohuaguoguoApplication.java
+++ src/main/java/com/huaguoguo/HellohuaguoguoApplication.java
@@ -1,10 +1,13 @@
 package com.huaguoguo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HellohuaguoguoApplication {
+
 
 	//哈哈哈
 	public static void main(String[] args) {
--- src/main/java/com/huaguoguo/controller/BaseController.java
+++ src/main/java/com/huaguoguo/controller/BaseController.java
@@ -1,14 +1,23 @@
 package com.huaguoguo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class BaseController {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello(){
+        logger.debug("hello");
+        logger.info("hello");
+
+
         return "hello huaguoguo";
     }
 }
