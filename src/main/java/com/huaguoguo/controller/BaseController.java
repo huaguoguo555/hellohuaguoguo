e2172c2aad1fd747f12607a3ccd6878a74a1bbdf
huaguoguo555 <huoguoguo404@163.com>
2018-04-17 10:48:38 +0800

    加入了生成图形验证码的功能

--- src/main/java/com/huaguoguo/controller/BaseController.java
+++ src/main/java/com/huaguoguo/controller/BaseController.java
@@ -1,8 +1,15 @@
 package com.huaguoguo.controller;
 
+import com.huaguoguo.util.VerifyUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.OutputStream;
 
 @RestController
 public class BaseController {
@@ -11,4 +18,7 @@ public class BaseController {
     public String hello(){
         return "hello huaguoguo";
     }
+
+
+
 }
---
+++ src/main/java/com/huaguoguo/controller/ValidateController.java
@@ -0,0 +1,32 @@
+package com.huaguoguo.controller;
+
+import com.huaguoguo.util.VerifyUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.OutputStream;
+
+@RestController
+@RequestMapping("/validate")
+public class ValidateController {
+
+    @RequestMapping(value = "/image",method = RequestMethod.GET)
+    public void valicode(HttpServletResponse response, HttpSession session) throws Exception{
+        //利用图片工具生成图片
+        //第一个参数是生成的验证码，第二个参数是生成的图片
+        Object[] objs = VerifyUtil.createImage();
+        //将验证码存入Session
+        session.setAttribute("imageCode",objs[0]);
+
+        //将图片输出给浏览器
+        BufferedImage image = (BufferedImage) objs[1];
+        response.setContentType("image/png");
+        OutputStream os = response.getOutputStream();
+        ImageIO.write(image, "png", os);
+    }
+}
